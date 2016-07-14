ad96024a3f92ad23fbedd86ceed5f147559196af
seethersan <carlos_jcez@hotmail.com>
2016-07-14 16:34:03 -0500

    Eliminando app home

--- twitter/settings.py
+++ twitter/settings.py
@@ -119,4 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATIC_URL = '/static/'
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates')
+)

8fe464618342853194337ad032ba504526e73e9e
seethersan <carlos_jcez@hotmail.com>
2016-07-14 16:38:49 -0500

    Adding twitlike app and configured settings

--- twitter/settings.py
+++ twitter/settings.py
@@ -56,7 +56,9 @@ ROOT_URLCONF = 'twitter.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,7 +124,3 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
-
-TEMPLATE_DIRS = (
-    os.path.join(BASE_DIR, 'templates')
-)
